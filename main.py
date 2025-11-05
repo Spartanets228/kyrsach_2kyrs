be879ff2f83612a48f21925369e4ce7f37adced4
Spartanets228 <maksgoncharov567@gmail.com>
2025-11-05 11:34:00 +0300

    345

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/start", response_class=HTMLResponse)
+def get_login_page(request: Request):
+    return templates.TemplateResponse("start.html", {"request": request})
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
