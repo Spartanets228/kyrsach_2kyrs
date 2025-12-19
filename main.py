40337b37280c4f10d1f52a173d2bc541c0fcac57
CyberGoblinV8 <maksgoncharov567@gmail.com>
2025-12-19 10:25:38 +0300

    12

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from urllib.parse import quote, unquote
+from urllib.parse import unquote
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
